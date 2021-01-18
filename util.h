7506bfe4b1a0de7c422b3b64cfb8db5afca5de5e
Kiëd Llaentenn <kiedtl@tilde.team>
2021-01-18 01:15:39 +0000

    util.h: update

--- util.h
+++ util.h
@@ -4,9 +4,13 @@
 #include <stdint.h>
 #include "list.h"
 
-#define UNUSED(VAR) ((void) (VAR))
-#define SIZEOF(ARR) (sizeof(ARR)/sizeof(*(ARR)))
-#define BITSET(V,B) (((V) & (B)) == (B))
+#define MAX(V,M)     ((V)>(M)?(M):(V))
+#define MIN(V,M)     ((V)<(M)?(M):(V))
+#define CLAMP(V,H,L) (MIN(MAX(V,H),L))
+#define CHKSUB(A,B)  (((A)-(B))>(A)?0:((A)-(B)))
+#define UNUSED(VAR)  ((void) (VAR))
+#define SIZEOF(ARR)  (sizeof(ARR)/sizeof(*(ARR)))
+#define BITSET(V,B)  (((V) & (B)) == (B))
 
 void die(const char *fmt, ...);
 char *format(const char *format, ...);
